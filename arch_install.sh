038d2c88295ab2b3e1a0e935a369363e8661b24c
Lars Jeppesen <lj@linel.dk>
2023-02-05 22:17:48 +0000

    Optional git crendtial manager

--- arch_install.sh
+++ arch_install.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 
 sudo pacman -S rofi rofimoji ttf-joypixels noto-fonts noto-fonts-emoji nautilus i3status-rust wireplumber iproute2 strace xautolock i3lock feh zsh curl tmux
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# yay -S git-credential-manager-core
